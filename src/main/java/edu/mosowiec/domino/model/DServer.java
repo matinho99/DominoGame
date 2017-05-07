5c16d7840f3ff3ce19eacce0f36c2078f71d2812
matinho99 <matinho99@gmail.com>
2017-05-07 13:43:48 +0200

    add obserer interface

--- src/main/java/edu/mosowiec/domino/model/DServer.java
+++ src/main/java/edu/mosowiec/domino/model/DServer.java
@@ -1,5 +1,13 @@
 package edu.mosowiec.domino.model;
 
-public class DServer {
+import java.util.Observable;
+import java.util.Observer;
+
+public class DServer implements Observer {
+
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
